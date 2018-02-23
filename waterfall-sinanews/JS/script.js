99dbd2cb7f89c9e72197ebcd132af2a71d2f2578
Lin-Ya <fenghuiwing@outlook.com>
2018-02-23 11:24:41 +0800

    实现懒加载和瀑布流布局

--- waterfall-sinanews/JS/script.js
+++ waterfall-sinanews/JS/script.js
@@ -4,16 +4,29 @@ var curPage = 1;
 
 var app = {
     init: function () {
-        this.bind();
+        this.isLoading = false
         this.start();
+        this.bind();
     },
     bind: function () {
         $('.box img').on('load', function () {
             app.waterfall();//这里绑定的是当图片载入完成以后再进行瀑布流布局，否则会获取不了box的准确高度
+            app.isToBottom();
+        });
+        $(window).on('scroll', function () {
+            if(app.isToBottom()){    //判断是否到达底部，结果为true则进行请求新的数据。
+                app.start();
+            }
+        })
+        $(window).on('resize',function () {
+            $('.box').removeAttr('style'); //去除原来样式，防止样式重叠，实现响应
+            app.waterfall();
         })
     },
     getData: function (callback) {
         var _this = this;
+        if(_this.isLoading) return;
+        _this.isLoading = true;
         $.ajax({
             url: 'http://platform.sina.com.cn/slide/album_tech',
             dataType: 'jsonp',
@@ -30,9 +43,13 @@ var app = {
                 ret.data.forEach(_this.createItem)
                 _this.bind();
                 curPage++
+                console.log('curPage' + curPage)
             } else {
                 console.log('get error data')
             }
+        })
+        .always(function () {
+            _this.isLoading = false;
         })
     },
     createItem: function (data) {
@@ -61,17 +78,22 @@ var app = {
                 $(value).css({
                     'position': 'absolute',
                     'top': minH + 'px',
-                    'left': miniHIndex * w + 'px'
+                    'left': miniHIndex * w + 'px',
+                    'float': 'left'
                 })
                 colHeightArray[miniHIndex] += $boxs.eq(index).outerHeight(); //注意，这里要对每一列的高度进行更新。
             }
         })
-        console.log(colHeightArray)
-        console.log(w)
-        console.log(colCount)
     },
     isToBottom: function () {
-        
+        if ($('.box').last().offset().top + 200 < $(window).height()) {  //判断最后一个区块是否完全显示在浏览器（因为浏览器太宽不出现滚动条）
+            app.start();
+        }
+        if ($('.box').last().offset().top <= $(window).height() + $(window).scrollTop()){
+            return true;
+        }else {
+            return false;
+        }
     },
     start: function () {
         var _this = this;
