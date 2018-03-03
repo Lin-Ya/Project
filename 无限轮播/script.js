52a14758a33aa92da299fb8c48558d657d373a01
Lin-Ya <fenghuiwing@outlook.com>
2018-03-03 16:06:59 +0800

    函数节流

--- 无限轮播/script.js
+++ 无限轮播/script.js
@@ -22,44 +22,67 @@ carousel.prototype = {
         })
         this.$clone_Last_Item = this.$img_li.eq(this.itemCount - 1).clone().addClass('cloneLast').prependTo(this.$img_ct);  //把最后一页克隆到第一页的前面，用于过度
         this.$clone_First_Item = this.$img_li.eq(0).clone().addClass('cloneFirst').appendTo(this.$img_ct);                  //把第一页克隆到最后一页后面。
+        this.switching = false;                                 //用于节流
         this.bind();                                            //实现绑定
     },
     //绑定事件
     bind: function () {
-        console.log(this.itemCount)
+        console.log('一共有 ' + this.itemCount + '页')
         var _this = this;
+        //绑定上下页切换事件
         this.$preBtn.on('click', function () {
+            if (_this.switching) {                                                   //函数节流
+                console.log('节流')
+                return;
+            }
             _this.pre();
         });
         this.$nextBtn.on('click', function () {
+            if (_this.switching) {                                                   //函数节流
+                console.log('节流')
+                return;
+            }
             _this.next();
         })
+
         //绑定轮播指示器的切换按钮功能
         this.$indicatorBtn.on('click', function (e) {
             var toThis = $(e.target).index()                                        //获取所点击的指示器按钮的下标
+            console.log(_this.switching)
+            if (_this.switching) {                                                   //函数节流
+                console.log('节流')
+                return;
+            }
+            _this.switching = true;                                                 //锁住状态                             
+            function updateStatus() {
+                console.log('ok')
+                _this.indexNow = toThis
+                _this.indicatorSwitch();                                            //对指示器进行更新  
+            }
             if(toThis > _this.indexNow) {                                           //判断目标下标距离当前下标的关系
                 _this.$img_ct.animate({
                     left: '-=' + _this.itemWidth * (toThis - _this.indexNow)        //如果目标下标>当前下标，表示想左滚动。   计算出来的值应该>0,方向由‘-=’来控制
-                },400,function () {
-                    _this.indexNow = toThis
-                    _this.indicatorSwitch();                                        //对指示器进行更新  
+                }, 350, updateStatus(),function () {
+                    _this.switching = false;
                 })
             }else {
                 _this.$img_ct.animate({
                     left: '+=' + _this.itemWidth * (_this.indexNow - toThis)        //如果目标下标<当前下标，表示想左滚动。   计算出来的值应该>0,方向由‘-=’来控制
-                }, 400, function () {
-                    _this.indexNow = toThis
-                    _this.indicatorSwitch();                                        //对指示器进行更新  
+                }, 350, updateStatus(), function () {
+                    _this.switching = false;
                 })
             }
         })
     },
-    //实现上一页
+
+    //实现上下一页
     pre: function () {
         var _this = this;
+        _this.switching = true;
         this.$img_ct.animate({
             left: '+=' + this.itemWidth
         },400,function () {
+            _this.switching = false;
             _this.indexNow --;
             if (_this.indexNow === -1) {                //-1表示，当前转到了克隆对象为最后一页的时候，表示来到了轮播队列的首位，这时候就是切换轮播到被克隆的最后一页。
                 _this.$img_ct.css({
@@ -71,12 +94,13 @@ carousel.prototype = {
             console.log(_this.indexNow)
         })
     },
-    //实现下一页
     next: function () {
         var _this = this;
+        _this.switching = true;
         this.$img_ct.animate({
             left: '-=' + this.itemWidth
         },400,function () {
+            _this.switching = false;
             _this.indexNow++;
             if (_this.indexNow === _this.itemCount) {     //这里表示，当前转到了克隆对象为第一页的时候，表示来到了轮播队列的尾部，这时候就是切换轮播到被克隆的最后一页。
                 _this.$img_ct.css({
@@ -88,6 +112,7 @@ carousel.prototype = {
             console.log(_this.indexNow)
         })
     },
+    
     //更新轮播指示器的状态
     indicatorSwitch: function () {
         this.$indicatorBtn.eq(this.indexNow).addClass('active').siblings().removeClass('active')
